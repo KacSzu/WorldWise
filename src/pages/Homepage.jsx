2ff6f07fa4938363a9d41930dcfcef7499a7c52b
KacSzu <szulckacper19@gmail.com>
2023-09-12 11:46:05 +0200

    starter files

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
+function Homepage() {
+  return (
+    <div>
+      <PageNav></PageNav>
+      <AppNav></AppNav>
+      <h1>worldwise</h1>
+      <Link to="/app">Go to the app</Link>
+    </div>
+  );
+}
+
+export default Homepage;
