701807d2f7cc7d11ce2f37aa7e0c622b79ffc2ed
KacSzu <szulckacper19@gmail.com>
2023-09-13 11:17:12 +0200

    Implementing the Cities List

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,47 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+const BASE_URL = "http://localhost:8000";
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        if (!res.ok) throw new Error("Something went wrong");
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        alert(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="app" element={<AppLayout />}>
-          <Route path="cities" element={<div>list of cities</div>}></Route>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          ></Route>
           <Route path="countries" element={<div>list of cntrys</div>}></Route>
+          <Route path="form" element={<div>form</div>}></Route>
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from "./Sidebar.module.css";
+import { Outlet } from "react-router-dom";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
 function Sidebar() {
@@ -6,7 +7,7 @@ function Sidebar() {
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>list of citeis</p>
+      <Outlet />
       <footer className={styles.footer}>gsdga</footer>
       <p className={styles.copyright}>
         &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
